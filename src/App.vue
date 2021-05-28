cf0eface311a285972d996a1a9708bc0472b38e2
v_fuxinyi <v_fuxinyi@tencent.com>
2021-05-28 20:53:36 +0800

    feat: 2021/5/28
    --date=2021/5/28

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,75 @@
 <template>
   <section class="app">
-      <input type="text" v-model="name" placeholder="请输入名字">
-      <button @click="alertName">弹出名字</button>
-      <button @click="alertId">弹出id</button>
-      <h1>id:{{id}}-name:{{name}}---age:{{age}}</h1>
-      <h1 ref="introduction">{{sayHi('男')}}</h1>
-      <my-button :type="type">我是按钮</my-button>
-      <button @click="type = 'error'">变红色</button>
+    <input type="text" v-model="name" placeholder="请输入名字" />
+    <button @click="alertName">弹出名字</button>
+    <button @click="alertId">弹出id</button>
+    <h1>id:{{ id }}-name:{{ name }}---age:{{ age }}</h1>
+    <h1 ref="introduction">{{ sayHi("男") }}</h1>
+    <my-button @click="btnClick" @customHandler="customHandler" :type="type"
+      >我是按钮</my-button
+    >
+    <button @click="type = 'error'">变红色</button>
+    <button @click="changeProvide">更改provide</button>
   </section>
 </template>
 
 <script>
-import { defineComponent, defineAsyncComponent, ref, reactive, toRefs, computed, watch } from 'vue'
+import {
+  defineComponent,
+  defineAsyncComponent,
+  ref,
+  reactive,
+  toRefs,
+  computed,
+  watch,
+  provide
+} from "vue";
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    MyButton: defineAsyncComponent(()=>import('@/components/myButton')),
+    // 异步组件
+    // MyButton: defineAsyncComponent(()=>import('@/components/myButton')),
+    MyButton: defineAsyncComponent({
+      loader: () => import("@/components/myButton"),
+    }),
   },
-  setup(){
-    let type = ref('info')
+  setup() {
+    let name = ref("Suk");
+    let job = ref("webEnginner");
+    provide("name", name);
+    provide("job", job);
+    const changeProvide = () => {
+      name.value = 'Susuk';
+      job.value = 'UI';
+    }
+    const customHandler = (args) => {
+      console.log("自定义事件被触发了", args);
+    };
+    let type = ref("info");
     const id = ref(1);
     const user = reactive({
-      name: 'Suk',
+      name: "Suk",
       age: 18,
-    })    
+    });
     const alertName = () => {
       alert(user.name);
       user.name = 123;
-    }
-    const alertId = () =>{
+    };
+    const alertId = () => {
       alert(id.value);
-    }
+    };
     const sayHi = computed(() => {
-      return gender => {
+      return (gender) => {
         return `${user.name}的性别是${gender},今年${user.age}岁`;
       };
     });
+    const btnClick = () => {
+      console.log("点击了自定义按钮");
+    };
     const introduction = ref(null);
-    watch(user, ()=>{
-      console.log(introduction.value.textContent)
-    })
+    watch(user, () => {
+      console.log(introduction.value.textContent);
+    });
     return {
       id,
       ...toRefs(user),
@@ -47,10 +77,13 @@ export default defineComponent({
       alertId,
       sayHi,
       introduction,
-      type
-    }
-  }
-})
+      type,
+      btnClick,
+      customHandler,
+      changeProvide
+    };
+  },
+});
 </script>
 
 <style lang="scss">
--- src/components/myButton.vue
+++ src/components/myButton.vue
@@ -1,41 +1,64 @@
 <template>
   <section class="myButton">
-      <button :class="type">
-        <slot></slot>
-      </button>
+    <button @mouseover="over" :class="type">
+      <slot></slot>
+    </button>
+    <p>name:{{ name }}</p>
+    <p>job:{{ job }}</p>
   </section>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 
 export default defineComponent({
-  name: 'MyButton',
-  setup({type}) {
+  name: "MyButton",
+  emits: {
+    customHandler: (str) => {
+      if (typeof str === "string") {
+        console.log(str);
+        return true;
+      }
+      console.warn("customHandler must be a string!");
+      return false;
+    },
+  },
+  setup(props, ctx) {
+    const name = inject("name");
+    const job = inject("job");
+    const over = () => {
+      ctx.emit("customHandler", "我是参数");
+      ctx.emit("customHandler", 123);
+    };
+    return {
+      over,
+      name,
+      job,
+    };
   },
   props: {
     type: {
-      type: String
+      type: String,
     },
-  }
-})
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-  .myButton {
-    button {
-      &.primary {
-        background-color: #0f0;
-      }
-      &.warning {
-        background-color: orange;
-      }
-      &.error {
-        background-color: #f00;
-      }
-      &.info {
-        background-color: #ccc;
-      }
+.myButton {
+  button {
+    &.primary {
+      background-color: #0f0;
+    }
+    &.warning {
+      background-color: orange;
+    }
+    &.error {
+      background-color: #f00;
+    }
+    &.info {
+      background-color: #ccc;
     }
   }
+}
 </style>
